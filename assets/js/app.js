8b5e1f8959e0ebe718ce58702b0720ac1854c5af
Maria <mariacutipa.gonzales@gmail.com>
2017-06-27 15:12:59 -0500

    list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $( _ => {
     var lista= "";
-    $.getJSON("http://laboratoria.cuadra.co:9339/api/v1/students/",function(data){
+    $.getJSON("https://laboratoria.cuadra.co:9339/api/v1/students/",function(data){
        $.each( data, function(i) {
             lista +="<li class='bg-black'>"+data[i].name+"</li>";
        });
